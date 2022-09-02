f534d77e5fa9b93c5afddbc393e22890b2c24532
Sienna Lloyd <sienna.lloyd@hey.com>
2022-09-02 00:02:00 -0600

    added transport tests, temporarily commented out some files.
    Signed-off-by: Sienna Lloyd <sienna.lloyd@hey.com>

---
+++ pkg/blaze/raft_transport_test.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 Anthropos Labs, Inc.
+ *
+ * Licensed under the PolyForm Strict License 1.0.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License here:
+ *  https://gitlab.com/anthropos-labs/pleiades/-/blob/mainline/LICENSE
+ */
+
+package blaze
+
+import (
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestRaftTransport(t *testing.T) {
+	suite.Run(t, new(RaftTransportTestSuite))
+}
+
+type RaftTransportTestSuite struct {
+	suite.Suite
+	logger zerolog.Logger
+}
