c9b0a6844161019986b4e8d43854a342946db52b
Sienna Lloyd <sienna@linux.com>
2022-09-14 20:11:41 -0600

    added rich errors and finished initial kv store cli
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- cmd/dev_bucket_create.go
+++ cmd/dev_bucket_create.go
@@ -26,13 +26,7 @@ import (
 // bucketCreateCmd represents the bucketGet command
 var bucketCreateCmd = &cobra.Command{
 	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "create a new bucket",
 	Run: bucketCreate,
 }
 
